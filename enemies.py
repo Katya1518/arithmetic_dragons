f3d1a2d7873f7521271dfb12ce52a0b1c1a5ad71
Katya1518 <beemouse@mail.ru>
2015-11-17 12:16:21 +0300

    Нужно доделалть троллей.

--- enemies.py
+++ enemies.py
@@ -2,6 +2,41 @@
 # license: GPLv3
 from gameunit import *
 from random import randint, choice
+def IsPrime(n):
+
+    d = 2
+
+    while n % d != 0:
+
+        d += 1
+
+    return d == n
+
+
+def Factor(n):
+
+    Ans = []
+
+    d = 2
+
+    while d * d <= n:
+
+        if n % d == 0:
+
+            Ans.append(d)
+
+            n //= d
+
+        else:
+
+            d += 1
+
+    if n > 1:
+
+        Ans.append(n)
+
+    return Ans
+
 
 class Enemy(Attacker):
     pass
@@ -16,6 +51,9 @@ def generate_random_enemy():
 def generate_dragon_list(enemy_number):
     enemy_list = [generate_random_enemy() for i in range(enemy_number)]
     return enemy_list
+def generate_troll_list(enemy_number):
+    enemy_list = [generate_random_enemy() for i in range(enemy_number)]
+    return enemy_list
 
 
 class Dragon(Enemy):
@@ -25,6 +63,52 @@ class Dragon(Enemy):
     def check_answer(self, answer):
         return answer == self.__answer
 
+class Troll(Enemy):
+    def set_answer(self, answer):
+        self._answer = answer
+    def check_answer(self, answer):
+        return answer == self.__answer
+
+
+class GuessTroll(Troll):
+    def __init__(self):
+        self._health = 50
+        self._attack = 5
+        self._color = 'грязный'
+    def question(self):
+        x = randint(1,5)
+        self.__quest = 'Угадай число от 1 до 5'
+        self.set_answer(x)
+        return self.__quest
+
+
+class EasyNumberTroll(Troll):
+    def __init__(self):
+        self._health = 60
+        self._attack = 5
+        self._color = 'очень грязный'
+
+
+
+
+    def question(self):
+        x = randint(1,50)
+        self.__quest = str(x) + 'это простое число?'
+        k = IsPrime(x)
+        self.set_answer(k)
+        return self.__quest
+
+class ComponentTroll(Troll):
+    def __init__(self):
+        self._health = 60
+        self._attack = 5
+        self._color = 'совсем грязный'
+    def question(self):
+        x = randint(1,100)
+        self.__quest = str(x)+', разложи мне его!!!'
+        n = Factor(x)
+        self.set_answer(n)
+        return self.__quest
 
 class GreenDragon(Dragon):
     def __init__(self):
@@ -39,6 +123,33 @@ class GreenDragon(Dragon):
         self.set_answer(x + y)
         return self.__quest
 
+
+class RedDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 13
+        self._color = 'красный'
+
+    def question(self):
+        x = randint(1,100)
+        y = randint(1,100)
+        self.__quest = str(x) + '-' + str(y)
+        self.set_answer(x - y)
+        return self.__quest
+
+
+class BlackDragon(Dragon):
+    def __init__(self):
+        self._health = 300
+        self._attack = 17
+        self._color = 'черный'
+
+    def question(self):
+        x = randint(1,100)
+        y = randint(1,100)
+        self.__quest = str(x) + '*' + str(y)
+        self.set_answer(x * y)
+        return self.__quest
 #FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
 # красный дракон учит вычитанию, а чёрный -- умножению.
 
--- tournament.py
+++ tournament.py
@@ -13,7 +13,7 @@ def annoying_input_int(message =''):
     return answer
 
 
-def game_tournament(hero, dragon_list):
+def game_tournament1(hero, dragon_list):
     for dragon in dragon_list:
         print('Вышел', dragon._color, 'дракон!')
         while dragon.is_alive() and hero.is_alive():
@@ -35,6 +35,30 @@ def game_tournament(hero, dragon_list):
         print('Ваш накопленный опыт:', hero._experience)
     else:
         print('К сожалению, Вы проиграли...')
+
+def game_tournament2(hero, dragon_list):
+    for dragon in dragon_list:
+        print('Вышел', dragon._color, 'дракон!')
+        while dragon.is_alive() and hero.is_alive():
+            print('Вопрос:', dragon.question())
+            answer = annoying_input_int('Ответ:')
+
+            if dragon.check_answer(answer):
+                hero.attack(dragon)
+                print('Верно! \n** дракон кричит от боли **')
+            else:
+                dragon.attack(hero)
+                print('Ошибка! \n** вам нанесён удар... **')
+        if dragon.is_alive():
+            break
+        print('Дракон', dragon._color, 'повержен!\n')
+
+    if hero.is_alive():
+        print('Поздравляем! Вы победили!')
+        print('Ваш накопленный опыт:', hero._experience)
+    else:
+        print('К сожалению, Вы проиграли...')
+
 
 def start_game():
 
